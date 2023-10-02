e934e99f0bd3be8937640b1a1b89d0644f8d00dd
magomed_n <naurbaev2020@gmail.com>
2023-10-02 18:41:40 +0300

    commented code to edit

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,175 +1,175 @@
-import { FC, useEffect, useState } from "react";
-import "./Products.scss";
-import { useNavigate, useParams } from "react-router-dom";
-import { apiStoreService } from "../../services/apiStoreService";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useForm } from "react-hook-form";
-import { useAppSelector } from "../../hooks/redux";
-import { baseUrl } from "../../consts";
+// import { FC, useEffect, useState } from "react";
+// import "./Products.scss";
+// import { useNavigate, useParams } from "react-router-dom";
+// import { apiStoreService } from "../../services/apiStoreService";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import * as yup from "yup";
+// import { useForm } from "react-hook-form";
+// import { useAppSelector } from "../../hooks/redux";
+// import { baseUrl } from "../../consts";
 
-const productFormSchema = yup
-  .object()
-  .shape({
-    name: yup.string().required(),
-    price: yup.string().required(),
-    description: yup.string().required(),
-  })
-  .required();
+// const productFormSchema = yup
+//   .object()
+//   .shape({
+//     name: yup.string().required(),
+//     price: yup.string().required(),
+//     description: yup.string().required(),
+//   })
+//   .required();
 
-type FormData = yup.InferType<typeof productFormSchema>;
+// type FormData = yup.InferType<typeof productFormSchema>;
 
-const Products: FC = () => {
-  const admin = useAppSelector((state) => {
-    return state.auth.admin;
-  });
+// const Products: FC = () => {
+//   const admin = useAppSelector((state) => {
+//     return state.auth.admin;
+//   });
 
-  const [selectedPreviewFile, setPreviewFile] = useState<File | null>(null);
-  const [selectedFile1, setSelectedFile1] = useState<File | null>(null);
-  const [selectedFile2, setSelectedFile2] = useState<File | null>(null);
-  const [selectedFile3, setSelectedFile3] = useState<File | null>(null);
+//   const [selectedPreviewFile, setPreviewFile] = useState<File | null>(null);
+//   const [selectedFile1, setSelectedFile1] = useState<File | null>(null);
+//   const [selectedFile2, setSelectedFile2] = useState<File | null>(null);
+//   const [selectedFile3, setSelectedFile3] = useState<File | null>(null);
 
-  const handleFileChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    setFunc: React.Dispatch<React.SetStateAction<File | null>>
-  ): void => {
-    setFunc(event.target.files![0]);
-  };
+//   const handleFileChange = (
+//     event: React.ChangeEvent<HTMLInputElement>,
+//     setFunc: React.Dispatch<React.SetStateAction<File | null>>
+//   ): void => {
+//     setFunc(event.target.files![0]);
+//   };
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: yupResolver(productFormSchema),
-  });
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<FormData>({
+//     resolver: yupResolver(productFormSchema),
+//   });
 
-  const { id } = useParams();
+//   const { id } = useParams();
 
-  const [postProduct, { isError }] = apiStoreService.useCreateProductMutation();
+//   const [postProduct, { isError }] = apiStoreService.useCreateProductMutation();
 
-  const submitForm = async (data: FormData) => {
-    const formData = new FormData();
-    formData.append("name", data.name);
-    formData.append("price", data.price);
-    formData.append("description", data.description);
-    formData.append("previewImage", selectedPreviewFile as File);
-    formData.append("image1", selectedFile1 as File);
-    formData.append("image2", selectedFile2 as File);
-    formData.append("image3", selectedFile3 as File);
-    formData.append("categoryId", id as string);
-    await postProduct(formData);
-    invalidateProducts();
-  };
+//   const submitForm = async (data: FormData) => {
+//     const formData = new FormData();
+//     formData.append("name", data.name);
+//     formData.append("price", data.price);
+//     formData.append("description", data.description);
+//     formData.append("previewImage", selectedPreviewFile as File);
+//     formData.append("image1", selectedFile1 as File);
+//     formData.append("image2", selectedFile2 as File);
+//     formData.append("image3", selectedFile3 as File);
+//     formData.append("categoryId", id as string);
+//     await postProduct(formData);
+//     invalidateProducts();
+//   };
 
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
-  const {
-    refetch: invalidateProducts,
-    data: products,
-    isLoading,
-    error,
-  } = apiStoreService.useFetchProductsByCategoryIdQuery(id);
+//   const {
+//     refetch: invalidateProducts,
+//     data: products,
+//     isLoading,
+//     error,
+//   } = apiStoreService.useFetchProductsByCategoryIdQuery(id);
 
-  useEffect(() => {
-    invalidateProducts();
-  }, []);
+//   useEffect(() => {
+//     invalidateProducts();
+//   }, []);
 
-  return (
-    <>
-      <div className="products-page-container">
-        {isLoading && <h1>идет загрузка</h1>}
-        {error && <h1>это ошибка</h1>}
-        {admin && (
-          <div className="add-product">
-            <input
-              {...register("name")}
-              placeholder="Name"
-              className="product-input"
-              type="text"
-            ></input>
+//   return (
+//     <>
+//       <div className="products-page-container">
+//         {isLoading && <h1>идет загрузка</h1>}
+//         {error && <h1>это ошибка</h1>}
+//         {admin && (
+//           <div className="add-product">
+//             <input
+//               {...register("name")}
+//               placeholder="Name"
+//               className="product-input"
+//               type="text"
+//             ></input>
 
-            <input
-              {...register("price")}
-              placeholder="Price"
-              type="text"
-              className="product-input"
-            ></input>
+//             <input
+//               {...register("price")}
+//               placeholder="Price"
+//               type="text"
+//               className="product-input"
+//             ></input>
 
-            <textarea
-              placeholder="Description"
-              {...register("description")}
-              name="description"
-              className="product-input"
-            />
-            <label className="choose-image">
-              Choose preview image
-              <input
-                onChange={(event) => handleFileChange(event, setPreviewFile)}
-                name="image"
-                type="file"
-                className="choose-image-input"
-              />
-            </label>
+//             <textarea
+//               placeholder="Description"
+//               {...register("description")}
+//               name="description"
+//               className="product-input"
+//             />
+//             <label className="choose-image">
+//               Choose preview image
+//               <input
+//                 onChange={(event) => handleFileChange(event, setPreviewFile)}
+//                 name="image"
+//                 type="file"
+//                 className="choose-image-input"
+//               />
+//             </label>
 
-            <label className="choose-image">
-              Choose image 1
-              <input
-                onChange={(event) => handleFileChange(event, setSelectedFile1)}
-                name="image"
-                type="file"
-                className="choose-image-input"
-              />
-            </label>
+//             <label className="choose-image">
+//               Choose image 1
+//               <input
+//                 onChange={(event) => handleFileChange(event, setSelectedFile1)}
+//                 name="image"
+//                 type="file"
+//                 className="choose-image-input"
+//               />
+//             </label>
 
-            <label className="choose-image">
-              Choose image 2
-              <input
-                onChange={(event) => handleFileChange(event, setSelectedFile2)}
-                name="image"
-                type="file"
-                className="choose-image-input"
-              />
-            </label>
+//             <label className="choose-image">
+//               Choose image 2
+//               <input
+//                 onChange={(event) => handleFileChange(event, setSelectedFile2)}
+//                 name="image"
+//                 type="file"
+//                 className="choose-image-input"
+//               />
+//             </label>
 
-            <label className="choose-image">
-              Chosse image 3
-              <input
-                onChange={(event) => handleFileChange(event, setSelectedFile3)}
-                name="image"
-                type="file"
-                className="choose-image-input"
-              />
-            </label>
+//             <label className="choose-image">
+//               Chosse image 3
+//               <input
+//                 onChange={(event) => handleFileChange(event, setSelectedFile3)}
+//                 name="image"
+//                 type="file"
+//                 className="choose-image-input"
+//               />
+//             </label>
 
-            <label className="submit-button" onClick={handleSubmit(submitForm)}>
-              Post Product
-            </label>
-          </div>
-        )}
-        <div className="product-container">
-          {products &&
-            products.map((item) => {
-              return (
-                <div
-                  key={item._id}
-                  onClick={() => {
-                    navigate(`/product/${item._id}`);
-                  }}
-                  className="product-card"
-                >
-                  <div className="product-image">
-                    <img src={baseUrl + item.previewImage?.lg} />
-                  </div>
-                  <h3 className="product-name">{item.name}</h3>
-                  <p className="price">{item.price} руб</p>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-    </>
-  );
-};
+//             <label className="submit-button" onClick={handleSubmit(submitForm)}>
+//               Post Product
+//             </label>
+//           </div>
+//         )}
+//         <div className="product-container">
+//           {products &&
+//             products.map((item) => {
+//               return (
+//                 <div
+//                   key={item._id}
+//                   onClick={() => {
+//                     navigate(`/product/${item._id}`);
+//                   }}
+//                   className="product-card"
+//                 >
+//                   <div className="product-image">
+//                     <img src={baseUrl + item.previewImage?.lg} />
+//                   </div>
+//                   <h3 className="product-name">{item.name}</h3>
+//                   <p className="price">{item.price} руб</p>
+//                 </div>
+//               );
+//             })}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Products;
+// export default Products;
