c1a8e5ae15deab125d12d3ef521c61e40c2062be
magomed_n <naurbaev2020@gmail.com>
2023-10-02 17:19:32 +0300

    fixed images error

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,23 +1,24 @@
 import { FC } from "react";
 import { images } from "../../assets/constants";
 import "./Loading.scss";
+import Image from "next/image";
 
 const Loading: FC = () => {
   return (
     <>
       <div className="loading-container">
         <div className="header-container">
-          <img src={images.vlogo} alt="" className="apple-logo" />
+          <Image src={images.vlogo} alt="" className="apple-logo" />
         </div>
         <div className="main-container">
-          <img className="iphone-14" src={images.iphone} alt="" />
+          <Image className="iphone-14" src={images.iphone} alt="" />
           <div className="flex">
             <h2>Apple</h2>
             <h3>Mania</h3>
           </div>
           <h4>вкусные технологии</h4>
         </div>
-        <img className="loader" src={images.loader} alt="" />
+        <Image className="loader" src={images.loader} alt="" />
       </div>
     </>
   );
