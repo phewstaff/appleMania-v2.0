f9421cbb0608e9936285f412547fd76ea310de05
magomed_n <naurbaev2020@gmail.com>
2023-10-17 21:08:25 +0300

    changed styles to tailwind

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
 import { images } from "@/assets/constants";
-import "./Loading.scss";
 import Image from "next/image";
 import { Loader2 } from "lucide-react";
 
 const Loading: FC = () => {
   return (
-    <div className="absolute left-1/2 top-1/2 z-50 h-full w-full -translate-x-1/2 -translate-y-1/2 transform bg-black text-white">
+    <div className="absolute left-1/2 top-1/2 z-50 min-h-full w-full -translate-x-1/2 -translate-y-1/2 transform bg-black text-white">
       <div className="flex w-full justify-center">
         <Image src={images.vlogo} alt="logo" />
       </div>
       <div className="flex flex-col items-center duration-1000 ease-out animate-in fade-in-0  zoom-in">
-        <Image className="iphone-14" src={images.iphone} alt="" />
+        <Image className="w-2/3 sm:w-2/6 md:w-1/4" src={images.iphone} alt="" />
         <div className="flex">
           <p>Apple</p>
           <p className="font-bold">Mania</p>
