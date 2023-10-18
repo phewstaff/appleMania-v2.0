f522f1cd232fc88e256788ecec37054ed438c5c9
magomed_n <naurbaev2020@gmail.com>
2023-10-18 15:19:10 +0300

    Delete product endpoint

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import Product from "@/models/product";
 import connectMongoDB from "@/libs/mongodb";
+import { utapi } from "uploadthing/server";
 
 export async function GET(req: NextRequest) {
   await connectMongoDB();
@@ -13,5 +14,30 @@ export async function GET(req: NextRequest) {
       { error: "Could not find products" },
       { status: 400 },
     );
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  await connectMongoDB();
+  const id = req.nextUrl.searchParams.get("id");
+  const key = req.nextUrl.searchParams.get("key");
+
+  try {
+    // Check if the ID is valid
+    if (!id || !key) {
+      throw new Error("Product ID or key is missing.");
+    }
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    await utapi.deleteFiles(key);
+
+    if (!deletedProduct) {
+      throw new Error("Product not found.");
+    }
+
+    return NextResponse.json({ message: "Product was deleted" });
+  } catch (error: any) {
+    console.error("Error deleting Product:", error);
+    return NextResponse.json({ error: error.message }, { status: 400 });
   }
 }
